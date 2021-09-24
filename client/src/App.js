a60d49e2ddc6712bbffd42e5df483cabb3a10943
Rachel McGrath <rachel.mcgrath@degreeanalytics.com>
2021-09-24 09:14:49 -0500

    apollo provider for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// import custom components/pages
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+// set up Apollo Provider
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
